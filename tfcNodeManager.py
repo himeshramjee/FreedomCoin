b8370cee6bc3aaba3a42830b55a6ee9787bb2d81
Himesh Ramjee <himeshramjee@users.noreply.github.com>
2018-09-06 22:19:47 +0200

    Added basic logger
    Script now logs to console and log file. Log file are created in same directory as the script and have usual datetime stamps. e.g. "tfc-node-manager-log-20180906-221405.log". So it will generate a new log file for every execution.

--- tfcNodeManager.py
+++ tfcNodeManager.py
@@ -5,11 +5,41 @@ from decimal import *
 import decimal
 import json
 from pathlib import Path, PureWindowsPath
-import sys
+import sys, traceback
 import os
+import time
 
-logging.basicConfig()
-logging.getLogger("TFC-RPC").setLevel(logging.DEBUG)
+# create logger
+datetimestamp = time.strftime("%Y%m%d-%H%M%S")
+logger=logging.getLogger('tfc-node-manager-log');
+logger.setLevel(logging.DEBUG);
+
+# create a file handler and set level to debug
+fh=logging.FileHandler("tfc-node-manager-log-{}.log".format(datetimestamp));
+fh.setLevel(logging.DEBUG);
+
+# create console handler and set level to debug
+ch=logging.StreamHandler();
+ch.setLevel(logging.DEBUG);
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s');
+
+# add formatter to ch
+ch.setFormatter(formatter);
+fh.setFormatter(formatter);
+
+# add ch to logger
+logger.addHandler(ch);
+logger.addHandler(fh);
+
+'''
+logger.debug('debug message');
+logger.info('info message');
+logger.warn('warn message');
+logger.error('error message');
+logger.critical('critical message');
+'''
 
 # Environment Setup
 class DecimalEncoder(json.JSONEncoder):
@@ -24,6 +54,9 @@ COIN_PRECISION=TFC_COIN_PRECISION;
 getcontext().prec=COIN_PRECISION;
 '''
 
+UNSPENT_MIN_CONF=50;
+UNSPENT_MAX_CONF=9999999;
+
 assert ('win32' in sys.platform), "This code runs on Windows only.";
 # print("Python {}".format(getcontext()));
 
@@ -32,11 +65,11 @@ coinConfFileName="Freedomcoin.conf";
 coinConfFileDefaultPath=Path("{}/Freedomcoin/{}".format(os.getenv("APPDATA"), coinConfFileName));
 coinConfFilePath=input("Enter the full file path to your coin config file (e.g. {}): ".format(coinConfFileDefaultPath));
 if len(coinConfFilePath.strip()) == 0:
-	print("\tTrying default location {}...".format(coinConfFileDefaultPath));
+	logger.info("Trying default location {}...".format(coinConfFileDefaultPath));
 	coinConfFilePath=coinConfFileDefaultPath;
 
 try:
-	print("Reading coin configuration file ({})...".format(coinConfFileName));
+	logger.info("Reading coin configuration file ({})...".format(coinConfFileName));
 	configBag={};
 	with open(coinConfFilePath, "r") as coinConfigFile:
 		for line in coinConfigFile:
@@ -46,9 +79,10 @@ try:
 			
 			k,v=line.split("=", 1);
 			configBag[k]=v;
+	logger.info("Done.");
 except:
-	print("Failed to read configuration file.");
-	print("Unexpected error:", sys.exc_info()[0]);
+	logger.critical("Failed to read configuration file.");
+	logger.critical("Unexpected error: {}".format(sys.exc_info()));
 	raise
 			
 rpcUser='';
@@ -56,7 +90,7 @@ if "rpcuser" in configBag:
 	rpcUser=configBag["rpcuser"];
 else:
 	# raise ValueError("RPC User ('rpcuser') configuration is missing");
-	print("\tRPC User ('rpcuser') configuration is missing. Aborting.");
+	logger.critical("RPC User ('rpcuser') configuration is missing. Aborting.");
 	exit();
 
 rpcPassword='';
@@ -64,7 +98,7 @@ if "rpcpassword" in configBag:
 	rpcPassword=configBag["rpcpassword"];
 else:
 	# raise ValueError("RPC Password ('rpcpassword') configuration is missing");
-	print("\tRPC Password ('rpcpassword') configuration is missing. Aborting.");
+	logger.critical("RPC Password ('rpcpassword') configuration is missing. Aborting.");
 	exit();
 
 rpcPort='';
@@ -72,7 +106,7 @@ if "rpcport" in configBag:
 	rpcPort=configBag["rpcport"];
 else:
 	# raise ValueError("RPC Port ('rpcport') configuration is missing");
-	print("\tRPC Port ('rpcport') configuration is missing. Aborting.");
+	logger.critical("RPC Port ('rpcport') configuration is missing. Aborting.");
 	exit();
 
 rpcIP='';
@@ -80,7 +114,7 @@ if "rpcip" in configBag:
 	rpcIP=configBag["rpcip"];
 else:
 	# raise ValueError("RPC IP ('rpcip') configuration is missing");
-	print("\tRPC IP ('rpcip') configuration is missing. Aborting.");
+	logger.critical("RPC IP ('rpcip') configuration is missing. Aborting.");
 	exit();
 
 masternodeCollateral='';
@@ -88,98 +122,102 @@ if "masternodeCollateral" in configBag:
 	masternodeCollateral=Decimal(configBag["masternodeCollateral"]);
 else:
 	#raise ValueError("Masternode collateral ('masternodeCollateral') configuration is missing");
-	print("\tMasternode collateral ('masternodeCollateral') configuration is missing. Aborting.");
+	logger.critical("Masternode collateral ('masternodeCollateral') configuration is missing. Aborting.");
 	exit();
 	
 # Setup rpc connection
 try:
-	print("Setting up node connection to {}...".format(rpcIP));
+	logger.info("Setting up node connection to {}...".format(rpcIP));
 	rpcConnection = AuthServiceProxy("http://%s:%s@%s:%s"%(rpcUser, rpcPassword, rpcIP, rpcPort));
-	print("Getting node information...");
-	pprint(rpcConnection.getinfo());
+	logger.info("Done.");
+	logger.info("Testing connection...");
+	rpcConnection.getinfo();
+	logger.info("Done.");
 except:
-	print("Failed to get node information.");
-	print("Unexpected error:", sys.exc_info()[0]);
+	logger.critical("Failed to get node information.");
+	logger.critical("Unexpected error: {}".format(sys.exc_info()));
 	raise
-	
-# pprint(rpcConnection.gettransaction(""));
-# pprint(rpcConnection.gettransaction(""));
-# pprint(rpcConnection.listunspent(1, 9999999));
-# pprint(rpcConnection.decodescript(""));
+
+if 0:
+	# pprint(rpcConnection.gettransaction("")); print();
+	# pprint(rpcConnection.gettransaction("")); print();
+	# pprint(rpcConnection.listunspent(UNSPENT_MIN_CONF, UNSPENT_MAX_CONF)); print();
+	# pprint(rpcConnection.decodescript(""));
+	exit();
 
 # Application setup
 # Holding addresses
 holdingAddressPrefix="holdingAddress";
 
 print();
-inputUser=input("Who's wallet do you want to process?");
+inputUser=input("Who's wallet do you want to process? ");
 inputUser=inputUser.lower().capitalize();
 if (len(inputUser.strip()) > 0):
-	print("\tChecking {} configuration for holding account address...".format(coinConfFileName));
+	logger.info("Checking {} configuration for holding account address...".format(coinConfFileName));
 	holdingAddressConfigName=holdingAddressPrefix + inputUser;
 	if holdingAddressConfigName in configBag:
 		holdingWalletAddress=configBag[holdingAddressConfigName];
 	else:
-		print("\tHolding Address configuration '{}' is missing. Aborting.".format(holdingAddressConfigName));
+		logger.error("Holding Address configuration '{}' is missing. Aborting.".format(holdingAddressConfigName));
 		exit();
 
-	print("\tFinding account for address {}...".format(holdingWalletAddress));
+	logger.info("Finding account for address {}...".format(holdingWalletAddress));
 	holdingWalletName=rpcConnection.getaccount(holdingWalletAddress);
 	if (holdingWalletName is not None):
-		print("\tCool {}, let's get started. This script will use {} as the holding wallet ({}).".format(inputUser, holdingWalletName, holdingWalletAddress));
+		logger.info("Cool {}, let's get started. This script will use {} as the holding wallet ({}).".format(inputUser, holdingWalletName, holdingWalletAddress));
 	else:
-		print("\tFailed to lookup account for user '{}'".format(inputUser));
+		logger.error("Failed to lookup account for user '{}'".format(inputUser));
 else:
-	print("Unrecognized user '{}'. Aborting.".format(inputUser));
+	logger.critical("Unrecognized user '{}'. Aborting.".format(inputUser));
 	exit();
 
 print();
-print("Processing all node accounts...");
+logger.info("Processing all node accounts...");
 walletAccounts=rpcConnection.listaccounts();
 allUnspent=[];
 totalUnspentAmount=0;
 for account in walletAccounts:
 	if len(account.strip()) == 0:
-		print("Ignoring unrecognised/invalid address ('{}')!\n".format(account));
+		logger.info("Ignoring unrecognised/invalid address ('{}')!\n".format(account));
 		continue;
 	else:
-		print("Account: {}".format(account));
+		logger.info("Account: {}".format(account));
 		
 	if account.endswith(inputUser) == False:
-		print("\tSkipping account. Doesn't belong to current user.\n");
+		logger.info("Skipping account. Doesn't belong to current user.\n");
 		# pprint("{} with balance {}".format(account, walletAccounts[account]));
 		continue;
 	elif account == holdingWalletName:
-		print("\tSkipping account. This is the holding address.\n");
+		logger.info("Skipping account. This is the holding address.\n");
 	else:
 		nodeBalance=walletAccounts[account];
 		if nodeBalance > masternodeCollateral:
-			unspentList=rpcConnection.listunspent(20, 9999999, rpcConnection.getaddressesbyaccount(account));
+			unspentList=rpcConnection.listunspent(UNSPENT_MIN_CONF, UNSPENT_MAX_CONF, rpcConnection.getaddressesbyaccount(account));
 			unspentBalance=Decimal(0);
 			if len(unspentList) > 0:
-				print("\tUnspent list contains {} outs (incl. collateral). Calculating balance without collateral...".format(len(unspentList)));
+				logger.info("Unspent list contains {} outs (incl. collateral). Calculating balance without collateral...".format(len(unspentList)));
 				# pprint(unspentList);
 				for otx in unspentList:
 					if otx['amount'] != masternodeCollateral:
-						print("\t\tAdding unspent amount of {} TFC.".format(Decimal(otx['amount'])));
+						logger.info("Adding unspent amount of {} TFC (txid: {}).".format(Decimal(otx['amount']), otx['txid']));
 						unspentBalance+=Decimal(otx['amount']);
 						allUnspent.append(otx);
 						totalUnspentAmount+=Decimal(otx['amount']);
-				print("\t{} TFC can be moved to holding address.\n".format(unspentBalance));
+				logger.info("{} TFC can be moved to holding address.\n".format(unspentBalance));
 		elif nodeBalance < masternodeCollateral:
-			print("\tWarning! This node address has insufficient collateral ({} vs expected {}).\n".format(nodeBalance, masternodeCollateral));
+			logger.warn("Warning! This node address has insufficient collateral ({} vs expected {}).\n".format(nodeBalance, masternodeCollateral));
 		else:
-			print("\tNothing to see here, keep scrolling.\n");
+			logger.info("Nothing to see here, keep scrolling.\n");
 
 # Estimate total tx fee using 0.0001 TFC per 1000 bytes which is rougly 6 txns
 totalTxFee=(len(allUnspent)/6)*0.00010000;
 totalTxFee=round(totalTxFee, 8);
 txAmount=round(totalUnspentAmount-Decimal(totalTxFee), 8);
 print();
-print("Holding wallet name is {} and it has address {}.".format(holdingWalletName, holdingWalletAddress));
+logger.info("Holding wallet name is {} and it has address {}.".format(holdingWalletName, holdingWalletAddress));
 acceptTxFeeInput=input("Using {} unspent txns valued at {} TFC will incur a fee of {} TFC. Continue? (y/n) ".format(len(allUnspent), txAmount, totalTxFee))	;
 if acceptTxFeeInput.strip().lower() != "y":
-	print("Tx not accepted by user. Aborting.");
+	logger.info("Tx not accepted by user. Aborting.");
 	exit();
 	
 # Gather tx inputs data
@@ -193,38 +231,38 @@ for u in allUnspent:
 
 # createrawtransaction [{"txid":txid,"vout":n},...] {address:amount,...}
 print();
-print("Creating tx hash going to address {}...".format(toAddresses));
+logger.info("Creating tx hash going to address {}...".format(toAddresses));
 createTxHex=rpcConnection.createrawtransaction(inputsList, toAddresses);
 createTxJson=rpcConnection.decoderawtransaction(createTxHex);
 if len(createTxHex.strip()) > 0:
 	# pprint(rpcConnection.decodescript(createTxHex));
 	# pprint(rpcConnection.decoderawtransaction(createTxHex));
 	createTxID=createTxJson["txid"];
-	print("\tTX was successfulling created. TxID is: {}".format(createTxID));
+	logger.info("TX was successfulling created. TxID is: {}".format(createTxID));
 else:
-	print("\tFailed to create tx. Aborting.");
+	logger.error("Failed to create tx. Aborting.");
 	exit();
 
 # signrawtransaction <hex string> [{"txid":txid,"vout":n,"scriptPubKey":hex,"redeemScript":hex},...] [<privatekey1>,...] [sighashtype="ALL"]
 print();
-print("Signing tx...");
+logger.info("Signing tx...");
 signTxResult=rpcConnection.signrawtransaction(createTxHex, inputsWithScriptKeysList);
 if signTxResult["complete"] == True:
-	print("\tSigning was successful.");
+	logger.info("Signing was successful.");
 	# pprint(rpcConnection.decodescript(signTxResult["hex"]));
 	# pprint(rpcConnection.decoderawtransaction(signTxResult));
 else:
-	print("\tSigning failed. Aborting.");
+	logger.error("Signing failed. Aborting.");
 	exit();
 
 # sendrawtransaction <hex string>
 print();
-print("Sending tx...");
+logger.info("Sending tx...");
 sendTxResult=rpcConnection.sendrawtransaction(signTxResult["hex"]);
 if len(sendTxResult.strip()) > 0:
-	print("\tSend tx result received...");
-	print("\tResulting TxID: {}".format(sendTxResult));
+	logger.info("Send tx result received...");
+	logger.info("Resulting TxID: {}".format(sendTxResult));
 else:
-	print("\tSend tx result is missing. Aborting.");
+	logger.error("Send tx result is missing. Aborting.");
 
-print("Total unspent was: {}".format(totalUnspentAmount));
+logger.info("Total unspent was {} TFC and tx fee paid was {}".format(totalUnspentAmount, totalTxFee));
